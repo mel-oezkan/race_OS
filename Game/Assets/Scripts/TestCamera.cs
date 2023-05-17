9dc4d4ac23e630aff67e7d0a9ab63f6ae052ef47
mel-oezkan <meloezkan@uni-osnabrueck.de>
2023-05-18 00:26:44 +0200

    removed small things & worked on driving

--- Game/Assets/Scripts/TestCamera.cs
+++ Game/Assets/Scripts/TestCamera.cs
@@ -3,15 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class TestCamera : MonoBehaviour
+
 {
 
-    private Vector3 offset = new Vector3(0f, 5f, -10f);
+    // declaring the offsets for the two camera modes
+    // third person where the camera follows the car
+    // eye in the sky where the camera is above the car
+    private static Vector3 offsetThirdPerson = new Vector3(0f, 2f, -2f);
+    private static Vector3 offsetEye = new Vector3(0f, 100f, 0);
+
+    // start with 3d person camera
+    private Vector3 offset = offsetThirdPerson;
+
     private bool isThirdPerson = true;
 
     [SerializeField] private Transform target;
     [SerializeField] private float translateSpeed;
     [SerializeField] private float rotationSpeed;
-
 
 
     private void FixedUpdate() 
@@ -23,12 +31,10 @@ public class TestCamera : MonoBehaviour
 
     private void HandleCameraMode () {
         if (Input.GetKeyDown(KeyCode.C)) {
-            if (isThirdPerson) {
-                offset = new Vector3(0f, 100f, 0);
-            } else {
-                offset = new Vector3(0f, 5f, -10f);
-
-            }
+            // changes the camera mode
+            offset = isThirdPerson 
+                ? offsetThirdPerson
+                : offsetEye;
 
             isThirdPerson = !isThirdPerson;
         }
