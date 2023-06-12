e31b7396eb00a295b1141921984b9e07c5395a99
mel-oezkan <meloezkan@uni-osnabrueck.de>
2023-06-12 23:49:46 +0200

    continued on shape editor (not relevant for the game, but maybe later for the road creation)

--- Game/Assets/Scripts/Utils/ShapeEditor.cs
+++ Game/Assets/Scripts/Utils/ShapeEditor.cs
@@ -8,6 +8,8 @@ public class ShapeEditor : Editor
 {
 
     ShapeCreator shapeCreator;
+    SelectionInfo selectionInfo;
+
     bool needsRepaint;
 
     void OnSceneGUI() 
@@ -40,14 +42,89 @@ public class ShapeEditor : Editor
             && guiEvent.modifiers == EventModifiers.None    
             // prevent perspective camera from adding points when moving camera
         ) {
-            
+            HandleLeftMouseDown(mousePosition);
+        }
+
+        if (
+            guiEvent.type == EventType.MouseUp
+            && guiEvent.button == 0 
+            && guiEvent.modifiers == EventModifiers.None    
+        ) {
+            HandleLeftMouseUp(mousePosition);
+        }
+
+        if (
+            guiEvent.type == EventType.MouseDrag
+            && guiEvent.button == 0 
+            && guiEvent.modifiers == EventModifiers.None    
+        ) {
+            HandleLeftMouseDrag(mousePosition);
+        }
+
+        // checks wether mouse is over a point 
+        // unless a point is already selected
+        if (!selectionInfo.pointIsSelected)  
+            UpdateMouseOverInfo(mousePosition);
+    } 
+
+    void HandleLeftMouseDown(Vector3 mousePosition)
+    {
+        // create a new point if mouse is not over existing one
+        if (!selectionInfo.mouseIsOverPoint)
+        {
             // handler such that points can be undone
             Undo.RecordObject(shapeCreator, "Add point");
-         
             shapeCreator.points.Add(mousePosition);
+            selectionInfo.pointIndex = shapeCreator.points.Count - 1;
+        }
+
+        selectionInfo.pointIsSelected = true;
+        selectionInfo.positionAtStartOfDrag = mousePosition;
+        needsRepaint = true;
+    }
+
+    void HandleLeftMouseUp(Vector3 mousePosition)
+    {
+        // remove selection when mouse is released
+        if (selectionInfo.pointIsSelected) {
+            shapeCreator.points[selectionInfo.pointIndex] = selectionInfo.positionAtStartOfDrag;
+            Undo.RecordObject(shapeCreator, "Move point");
+            shapeCreator.points[selectionInfo.pointIndex] = mousePosition;
+
+            selectionInfo.pointIsSelected = false;
+            selectionInfo.pointIndex = -1;
             needsRepaint = true;
         }
-    } 
+    }
+
+    void HandleLeftMouseDrag(Vector3 mousePosition)
+    {
+        if (selectionInfo.pointIsSelected) 
+        {
+            shapeCreator.points[selectionInfo.pointIndex] = mousePosition;
+        }
+    }
+
+    void UpdateMouseOverInfo(Vector3 mousePosition) 
+    {
+        int mouseOverPointIndex = -1;
+
+        for (int i = 0; i < shapeCreator.points.Count; i++) {
+            if (Vector3.Distance(mousePosition, shapeCreator.points[i]) < shapeCreator.handleRadius ) {
+                mouseOverPointIndex = i;
+                break;
+            }
+        }
+
+        // index changed
+        if (mouseOverPointIndex != selectionInfo.pointIndex) {
+            selectionInfo.pointIndex = mouseOverPointIndex;
+            selectionInfo.mouseIsOverPoint = mouseOverPointIndex != -1;
+            
+            // repaint the new selection
+            needsRepaint = true;
+        }
+    }
 
     void Draw() 
     {   
@@ -55,8 +132,13 @@ public class ShapeEditor : Editor
             Vector3 nextPoint = shapeCreator.points[(i+1) % shapeCreator.points.Count];
             Handles.color = Color.black;
             Handles.DrawDottedLine(shapeCreator.points[i], nextPoint, 4);
-            Handles.color = Color.white;
-            Handles.DrawSolidDisc(shapeCreator.points[i], Vector3.up, 0.5f);
+            
+            if (i == selectionInfo.pointIndex) 
+                Handles.color = selectionInfo.pointIsSelected ? Color.black : Color.red;
+            else
+                Handles.color = Color.white;
+    
+            Handles.DrawSolidDisc(shapeCreator.points[i], Vector3.up, shapeCreator.handleRadius);
         }
         needsRepaint = true;
     }
@@ -64,6 +146,20 @@ public class ShapeEditor : Editor
     void OnEnable() 
     {
         shapeCreator = target as ShapeCreator;
+        selectionInfo = new SelectionInfo();
+    }
+
+    public class SelectionInfo 
+    {
+        public int pointIndex;
+        public bool mouseIsOverPoint;
+        public bool pointIsSelected;
+        public Vector3 positionAtStartOfDrag;
+
+
+        public int lineIndex;
+        public bool mouseIsOverLine;
     }
 
 }
+
