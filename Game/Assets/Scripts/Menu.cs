9bba9f05b1ee96f6ee01b29affd8c8ed36a33e21
jojofdgshajk <93261504+jojofdgshajk@users.noreply.github.com>
2023-06-26 22:47:19 +0200

    menu quit button

--- Game/Assets/Scripts/Menu.cs
+++ Game/Assets/Scripts/Menu.cs
@@ -13,6 +13,12 @@ public class Menu : MonoBehaviour
 
     public void OnQuitButton()
     {
-        Application.Quit();
+        #if UNITY_EDITOR
+                        // Code to handle quitting in the Unity Editor (optional)
+                        UnityEditor.EditorApplication.isPlaying = false;
+        #else
+                // Code to handle quitting in a built version
+                Application.Quit();
+        #endif
     }
 }
