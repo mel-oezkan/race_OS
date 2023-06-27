961d00237304eb3e36b00a40826986d6353bc109
mel-oezkan <meloezkan@uni-osnabrueck.de>
2023-06-27 18:29:30 +0200

    minior code change

--- Game/Assets/Scripts/CarCode/CamController.cs
+++ Game/Assets/Scripts/CarCode/CamController.cs
@@ -5,27 +5,19 @@ using UnityEngine;
 public class CamController : MonoBehaviour
 {
    
-    
-    [SerializeField]
-    private Rigidbody carRB;
-
-
-    [SerializeField]
-    private Transform car;
-
-    [SerializeField]
-    private Vector3 offset;
-
-
+    [SerializeField] private Rigidbody carRB;
+    [SerializeField] private Transform car;
+    [SerializeField] private Vector3 offset;
 
     private void Start() 
     {
         carRB = car.GetComponent<Rigidbody>();
     }
 
-    
+    // updated after all other updates
     private void LateUpdate() 
-    {
+    {   
+        // calculate the car speed for dynamic camera movement
         float carSpeed = Vector3.Dot(car.forward, carRB.velocity);
 
         transform.position = Vector3.Lerp(
